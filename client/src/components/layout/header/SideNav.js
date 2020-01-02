6f2168b1b53029cbe01ceddb5941f83ff8f383a1
Anirudh <anirudhganwal06@gmail.com>
2020-01-02 23:55:53 +0530

    crated sideNavbar

--- client/src/components/layout/header/SideNav.js
+++ client/src/components/layout/header/SideNav.js
@@ -1,22 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { signedInLinks, signedOutLinks } from "./links";
 
 const sideNav = props => {
+  let links = [];
+  let importedLinks = [];
+  if (props.isSignedIn) {
+    importedLinks = signedInLinks;
+  } else {
+    importedLinks = signedOutLinks;
+  }
+
+  for (let i = 0; i < importedLinks.length; i++) {
+    links.push(
+      <Link to={importedLinks[i].url} key={i}>
+        <div className="sideNavLink">{importedLinks[i].name}</div>
+      </Link>
+    );
+  }
+
   return (
-    <div className="leftNavbarContainer">
-      <div className="leftNavbar">
-        <div className="row">
-          <div className="col-12">
-            <p>Login</p>
-          </div>
-          <div className="col-12">
-            <p>Signup</p>
-          </div>
+    <div className="sideNavContainer">
+      <div className="sideNav">
+        <div className="sideNavHeading vCenterContents hCenterContents">
+          <h1>My Account</h1>
         </div>
+        <div className="sideNavLinks">{links}</div>
       </div>
-      <div
-        className="leftNavbarEmptySpace"
-        onClick={props.closeSideNav}
-      ></div>
+      <div className="sideNavEmptySpace" onClick={props.closeSideNav}></div>
     </div>
   );
 };
