b2cd65cdfe19fb5deed087adcaa6b5744ef8e902
Rekhansh Priyadarshi <priyadarshirekhansh@gmail.com>
2020-01-04 03:32:48 +0530

    MyProfile linked with firebase

--- client/src/components/user/MyProfile.js
+++ client/src/components/user/MyProfile.js
@@ -1,59 +1,102 @@
 import React, { Component } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { withFirebase, firestoreConnect, isLoaded } from "react-redux-firebase";
+
 import DeliveryAddressCard from "../common/DeliveryAddressCard";
 
 class MyProfile extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      name: "Anirudh",
-      email: "anirudhganwal06@gmail.com",
-      mobile: "9468453223",
-      addresses: [
-        {
-          line1: "Opposite Bhagwati Hospital",
-          line2: "Kosli",
-          city: "Rewari",
-          pincode: "123302"
-        },
-        {
-          line1: "Hostel 10",
-          line2: "NIT Kurukshetra",
-          city: "Kurukshetra",
-          pincode: "136119"
-        }
-      ],
-      errors: {}
+      name: props.user.displayName,
+      email: props.user.email,
+      errors: {},
+      loaded: false
     };
   }
 
-  // onSubmit = e => {
-  //   e.preventDefault();
-  //   console.log("form submit");
-  // };
+  static getDerivedStateFromProps(props, state) {
+    return (isLoaded(props.user) && !state.loaded) ? {
+      name: props.user.displayName,
+      email: props.user.email,
+      errors: state.errors,
+      loaded: true
+    } : state;
+  }
 
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+  }
+
+  updateProfile = (e, data) => {
+    e.preventDefault();
+    const firebase = this.props.firebase;
+    const errors = {};
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+
+    switch(data) {
+      case "name":
+        if(name.checkValidity())
+          firebase.updateProfile({
+            displayName: name.value
+          });
+        else {
+          errors.name = "Enter a valid username";
+
+          this.setState({ errors });
+        }
+        break;
+    
+      case "email":
+        if(email.checkValidity())
+          firebase.updateEmail(email.value, true)
+            .then(() => firebase.reloadAuth())
+            .catch(err => {
+              switch(err.code) {
+                case "auth/email-already-in-use":
+                  errors.email = "Email already in use";
+                  this.setState({ errors });
+                  break;
+
+                case "auth/requires-recent-login":
+                  this.props.history.push("/login");
+                  break;
+
+                default:
+                  console.log(err);
+              }
+            });
+        else {
+          errors.email = "Enter a valid email";
+
+          this.setState({ errors });
+        }
+        break;
+    }
   };
 
-  deleteAddress = (e, index) => {
+  deleteAddress = (e, address) => {
     e.preventDefault();
-    let newAddresses = [...this.state.addresses];
-    newAddresses.splice(index, 1);
-    this.setState({ addresses: newAddresses });
+    this.props.firestore.delete({
+      collection: "users/" + this.props.uid + "/addresses",
+      doc: address
+    });
   };
 
   render() {
     const deliveryAddresses = [];
-    const addresses = this.state.addresses;
-    for (let i = 0; i < addresses.length; i++) {
+
+    for (const address in this.props.addresses) {
       deliveryAddresses.push(
         <DeliveryAddressCard
-          key={i}
-          index={i}
-          line1={addresses[i].line1}
-          line2={addresses[i].line2}
-          city={addresses[i].city}
-          pincode={addresses[i].pincode}
+          key={address}
+          index={address}
+          line1={this.props.addresses[address].line1}
+          line2={this.props.addresses[address].line2}
+          city={this.props.addresses[address].city}
+          pincode={this.props.addresses[address].pincode}
           delete={true}
           deleteAddress={this.deleteAddress}
           edit={true}
@@ -78,14 +121,17 @@ class MyProfile extends Component {
                   type="text"
                   name="name"
                   placeholder="Enter full name"
-                  value={this.state.name}
                   onChange={this.onChange}
+                  value={this.state.name}
                 />
-                <button className="input-group-append btn themeColorHoverBtn ml-2">
-                  Save
+                <button 
+                  className="input-group-append btn themeColorHoverBtn ml-2"
+                  onClick={e => this.updateProfile(e, "name")}
+                >
+                    Save
                 </button>
               </div>
-              <label htmlFor="email">Email Id</label>
+              <label htmlFor="email">Email ID</label>
               <div className="input-group mb-3">
                 <input
                   className="form-control"
@@ -93,11 +139,14 @@ class MyProfile extends Component {
                   type="email"
                   name="email"
                   placeholder="Enter email id"
-                  value={this.state.email}
                   onChange={this.onChange}
+                  value={this.state.email}
                 />
-                <button className="input-group-append btn themeColorHoverBtn ml-2">
-                  Save
+                <button 
+                  className="input-group-append btn themeColorHoverBtn ml-2"
+                  onClick={e => this.updateProfile(e, "email")}
+                >
+                    Save
                 </button>
               </div>
               <label htmlFor="mobile">Mobile Number</label>
@@ -108,8 +157,7 @@ class MyProfile extends Component {
                   type="text"
                   name="mobile"
                   placeholder="Enter mobile number"
-                  value={this.state.mobile}
-                  onChange={this.onChange}
+                  value={this.props.user.mobileNo}
                   disabled={true}
                 />
               </div>
@@ -123,4 +171,19 @@ class MyProfile extends Component {
   }
 }
 
-export default MyProfile;
+const getQuery = props => [{
+  collection: "users/" + props.uid + "/addresses",
+  storeAs: "addresses"
+}];
+
+const mapStateToProps = state => ({
+  uid: state.firebase.auth.uid,
+  user: state.firebase.profile,
+  addresses: state.firestore.data.addresses
+});
+
+export default compose(
+  withFirebase,
+  connect(mapStateToProps),
+  firestoreConnect(getQuery)
+)(MyProfile);
