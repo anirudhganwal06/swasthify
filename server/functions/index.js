226a3fe43033393feade29c2f909b2461d8f00b3
Rekhansh Priyadarshi <priyadarshirekhansh@gmail.com>
2020-01-20 00:07:24 +0530

    placeOrder cloud function added

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,49 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const paytmChecksum = require('./paytm/checksum');
+const paytmConfig = require('./paytm/config');
+
+admin.initializeApp();
+
+exports.placeOrder = functions.https.onRequest(async (req, res) => {
+  const fs = admin.firestore();
+  
+  console.log(req.body);
+  const paytmParams = {};
+  paytmParams['MID'] = paytmConfig.MID;
+  paytmParams['ORDER_ID'] = req.query.ORDER_ID;
+  paytmParams['CUST_ID'] = 'CUST0001';
+  paytmParams['INDUSTRY_TYPE_ID'] = paytmConfig.INDUSTRY_TYPE_ID;
+  paytmParams['CHANNEL_ID'] = paytmConfig.CHANNEL_ID;
+  paytmParams['TXN_AMOUNT'] = '1.00';
+  paytmParams['WEBSITE'] = paytmConfig.WEBSITE;
+  paytmParams['CALLBACK_URL'] = 'https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp';
+  paytmParams['EMAIL'] = 'abc@gmail.com';
+  paytmParams['MOBILE_NO'] = '9999999999';
+  paytmChecksum.genchecksum(paytmParams, paytmConfig.MERCHANT_KEY, (err, checksum) => {
+    if(err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+    console.log('Checksum: ', checksum, "\n");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>Merchant Checkout Page</title>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<center><h1>Please do not refresh this page...</h1></center>');
+    res.write('<form method="post" action="' + paytmConfig.URL + '" name="paytm_form">');
+    for(var x in paytmParams)
+      res.write('<input type="hidden" name="' + x + '" value="' + paytmParams[x] + '">');
+    res.write('<input type="hidden" name="CHECKSUMHASH" value="' + checksum + '">');
+    res.write('</form>');
+    res.write('<script type="text/javascript">');
+    res.write('document.paytm_form.submit();');
+    res.write('</script>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  });
+});
