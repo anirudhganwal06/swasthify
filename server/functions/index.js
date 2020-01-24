640a826f881ceff2ba75142fb878ed3d68e505e6
Karan Gupta <karangupta199920@gmail.com>
2020-01-24 21:31:56 +0530

    calcCartTotal cloud function amodified

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,4 +2,4 @@ const functions = require("firebase-functions");
 
 exports.placeOrder = functions.https.onRequest(require("./routes/placeOrder"));
 exports.verifyOrder = functions.https.onRequest(require("./routes/verifyOrder"));
-exports.calcCartTotal = functions.https.onRequest(require("./routes/calcCartTotal"));
+exports.calcCartTotal = require("./routes/calcCartTotal");
--- server/functions/routes/calcCartTotal.js
+++ server/functions/routes/calcCartTotal.js
@@ -1,7 +1,36 @@
 const functions = require('firebase-functions');
 const fs = require('../firebase').firestore();
 
-module.exports = functions.firestore.document('users/{userId}/cart')
-    .onUpdate((change, context) => {
-        console.log("YAS");
-    });
+module.exports = functions.firestore.document('users/{userId}')
+  .onUpdate(async (change, context) => {
+    if(JSON.stringify(change.after.data().cart) !== JSON.stringify(change.before.data().cart)) {
+      
+      const promises = [];
+      const data = change.after.data().cart.products;
+      const discount= change.after.data().cart.discount;
+
+      for (const product in data)
+        promises.push(fs.doc('products/' + product).get());
+      
+      let total = 0;
+      
+      const result = await Promise.all(promises);
+      
+      result.forEach(actualProduct => {
+        for (const product in data) {
+          if (product === actualProduct.id) {
+            for (const variant in data[product]) {
+              total += actualProduct.data().variants[variant].actualPrice * data[product][variant];
+            }
+          }
+        }
+      });
+      
+      return fs.doc('users/' + context.params.userId).update({
+        "cart.subTotal": total,
+        "cart.total": total - discount
+      });
+    } else {
+      return null;
+    }
+  });
