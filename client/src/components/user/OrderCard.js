c60da52891d637c421046e0b550ebd8ec5d5225a
Rekhansh Priyadarshi <priyadarshirekhansh@gmail.com>
2020-01-27 00:48:42 +0530

    Orders Page linked with firebase

--- client/src/components/user/OrderCard.js
+++ client/src/components/user/OrderCard.js
@@ -1,98 +1,82 @@
-import React, { Component } from "react";
+import React from "react";
 import { withRouter, Link } from "react-router-dom";
 import { compose } from "redux";
 import { connect } from "react-redux";
-import { withFirestore } from "react-redux-firebase";
+import loading from "../common/Loading";
 
-class OrderCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: "SWF0000001",
-      products: [
-        {
-          title: "Haldi Powder",
-          imageUrl:
-            "https://firebasestorage.googleapis.com/v0/b/swasthify-6d5c2.appspot.com/o/products%2Fwheat.jpg?alt=media&token=beb58574-5f00-42b1-ae78-9d8ec1fff538",
-          price: "499"
-        },
-        {
-          title: "Haldi Powder",
-          imageUrl:
-            "https://firebasestorage.googleapis.com/v0/b/swasthify-6d5c2.appspot.com/o/products%2Fwheat.jpg?alt=media&token=beb58574-5f00-42b1-ae78-9d8ec1fff538",
-          price: "499"
-        }
-      ],
-      orderDate: "12 September, 2019",
-      deliveryStatus: "delivered",
-      deliveryDate: "14 September, 2019",
-      paymentStatus: "Succeed",
-      totalAmount: "900"
-    };
-  }
+const OrderCard = props => {
+  const order = props.order;
 
-  render() {
-    const order = this.state;
+  const productsList = [];
 
-    const productsList = [];
-
-    for (let i in order.products) {
+  for (const i in order.products) {
+    for(const v in order.products[i])
       productsList.push(
-        <div className="cartProductCard">
+        <div key={i} className="cartProductCard">
           <div className="row">
-            <div className="col-2 colInRow">
-              <Link to="/product/flour-wheat">
-                <div className="imageContainer">
-                  <img src={order.products[i].imageUrl} alt="Product " />
+            {(props.products && props.products[i]) ? (
+              <React.Fragment>
+                <div className="col-2 colInRow">
+                  <Link to={"/product/" + i}>
+                    <div className="imageContainer">
+                      <img src={props.products[i].image} alt="Product " />
+                    </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>
-            <div className="col-10 colInRow">
-              <p>{order.products[i].title}</p>
-              <p className="text-muted">{"Rs. " + order.products[i].price}</p>
-            </div>
+                <div className="col-10 colInRow">
+                  <p>{props.products[i] && props.products[i].name}</p>
+                  <small className="text-muted">
+                    {props.products[i].variants[v].size + " " + props.products[i].unit}
+                  </small>
+                  <p className="bold">
+                    {"₹ " + order.products[i][v].price + " "}
+                    <span>&times;</span>
+                    {" " + order.products[i][v].quantity}
+                  </p>
+                </div>
+              </React.Fragment>) : loading()}
           </div>
         </div>
       );
-    }
-
-    return (
-      <div className="card my-3">
-        <div className="card-header">
-          <span className="btn btn-primary py-0">{order.id}</span>
-          <p className="float-right">
-            {order.deliveryStatus === "delivered" ? (
-              <span>
-                <span className="text-muted">Delivered on </span>
-                {order.deliveryDate}
-              </span>
-            ) : (
-              <span>
-                <span className="text-muted">Delivery: </span>
-                <span className="text-capitalize">{order.deliveryStatus}</span>
-              </span>
-            )}
-          </p>
-        </div>
-        <div className="card-body">
-          {productsList}
-        </div>
-        <div className="card-header">
-          <p className="float-left">{"Ordered on " + order.orderDate}</p>
-          <p className="float-right">
-            <span className="text-muted">Order Total: </span>
-            {"Rs. " + order.totalAmount}
-          </p>
-        </div>
-      </div>
-    );
   }
-}
 
-const mapStateToProps = () => ({});
+  return (
+    <div className="card my-3">
+      <div className="card-header">
+        <span className="btn btn-primary py-0">{props.orderid}</span>
+        <p className="float-right">
+          {order.deliveryStatus === "delivered" ? (
+            <span>
+              <span className="text-muted">Delivered on </span>
+              {order.deliveryDate}
+            </span>
+          ) : (
+            <span>
+              <span className="text-muted">Delivery: </span>
+              <span className="text-capitalize">{order.deliveryStatus}</span>
+            </span>
+          )}
+        </p>
+      </div>
+      <div className="card-body">
+        {productsList}
+      </div>
+      <div className="card-header">
+        <p className="float-left">{"Ordered on " + order.orderDate.toDate().toDateString()}</p>
+        <p className="float-right">
+          <span className="text-muted">Order Total: </span>
+          {"₹ " + order.total}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  products: state.firestore.data.products
+});
 
 export default compose(
-  withFirestore,
   withRouter,
   connect(mapStateToProps)
 )(OrderCard);
--- client/src/components/user/OrdersList.js
+++ client/src/components/user/OrdersList.js
@@ -1,15 +1,62 @@
 import React, { Component } from "react";
 import OrderCard from "../user/OrderCard";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect, isLoaded } from "react-redux-firebase";
 
-export default class OrdersList extends Component {
+import loading from "../common/Loading";
+
+class OrdersList extends Component {
+  ids = [];
+
+  getProducts() {
+    const productIds = new Set();
+
+    for(const order in this.props.orders)
+      Object.keys(this.props.orders[order].products).forEach(key => productIds.add(key));
+
+    for(const id of productIds.values())
+      if(!this.ids.includes(id)) {
+        this.ids.push(id);
+        this.props.firestore.get({ collection: "products", doc: id});
+      }
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props !== prevProps)
+      this.getProducts();
+  }
+
   render() {
+    const orderCards = [];
+
+    for(const order in this.props.orders) {
+      orderCards.push(
+        <OrderCard key={order} orderid={order} order={this.props.orders[order]} />
+      );
+    }
+
     return (
       <div className="container p-3">
         <h1 className="text-center">My Orders</h1>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {isLoaded(this.props.orders) ? orderCards : loading()}
       </div>
     );
   }
 }
+
+const getQuery = props => props.orderIds.map(id => ({
+  collection: "orders",
+  doc: id
+}));
+
+const matchStateToProps = state => ({
+  orderIds: state.firebase.profile.orders,
+  orders: state.firestore.data.orders,
+  products: state.firestore.data.products || {}
+});
+
+export default compose(
+  connect(matchStateToProps),
+  firestoreConnect(getQuery)
+)(OrdersList);
