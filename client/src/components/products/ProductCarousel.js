6a171e12b8a0f97537ba1d2a8c811fd7ee3bef1e
Anirudh <anirudhganwal06@gmail.com>
2020-02-06 19:08:52 +0530

    done

--- client/src/components/products/ProductCarousel.js
+++ client/src/components/products/ProductCarousel.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
@@ -11,32 +9,16 @@ import ProductCard from "./ProductCard";
 import loading from "../common/Loading";
 
 const ProductCarousel = props => {
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 992 },
-      items: 4
-    },
-    desktop: {
-      breakpoint: { max: 991, min: 768 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 767, min: 501 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 500, min: 0 },
-      items: 1
-    }
-  };
-
   let productCardContainers = [];
   let productsPresent = false;
   if (isLoaded(props.products)) {
     if (!isEmpty(props.products)) {
       for (let i in props.products) {
         productCardContainers.push(
-          <div className="p-1 h-100" key={props.products[i].id}>
+          <div
+            className="p-1 h-100 float-left carouselProductCard"
+            key={props.products[i].id}
+          >
             <ProductCard
               collection={props.category}
               productId={props.products[i].id}
@@ -50,31 +32,40 @@ const ProductCarousel = props => {
     }
   }
   return (
-    <div className={classnames("container", {"d-none": !productsPresent})}>
-        <div className="productCarouselContainer">
-          <div className="clearfix">
-            <h3 className="text-capitalize mb-1 float-left">
-              {props.category + "s" || props.tag}
-            </h3>
-            <Link
-              to={
-                "/products?" +
-                (props.category ? "category=" : "tag=") +
-                props.category
-              }
-            >
-              <button className="btn themeColorHoverBtn btn-sm float-right">
-                See All
-              </button>
-            </Link>
-          </div>
-          <Carousel
-            responsive={responsive}
+    <div className={classnames("container", { "d-none": !productsPresent })}>
+      <div className="productCarouselContainer">
+        <div className="clearfix">
+          <h3 className="text-capitalize mb-1 float-left">
+            {props.category + "s" || props.tag}
+          </h3>
+          <Link
+            to={
+              "/products?" +
+              (props.category ? "category=" : "tag=") +
+              props.category
+            }
           >
-            {productCardContainers}
-          </Carousel>
-          {isLoaded(props.products) ? "" : loading("80px")}
+            <button className="btn themeColorHoverBtn btn-sm float-right">
+              See All
+            </button>
+          </Link>
         </div>
+        {isLoaded(props.products) ? (
+          <div
+            className="productCarouselMain"
+            // responsive={responsive}
+          >
+            <div
+              className="productCarouselMainScroll"
+              style={{ width: props.products.length * 274 + "px" }}
+            >
+              {productCardContainers}
+            </div>
+          </div>
+        ) : (
+          loading("80px")
+        )}
+      </div>
     </div>
   );
 };
