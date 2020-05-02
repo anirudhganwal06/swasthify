6765416b9a426959fda0bd4fa0a62ccf693e81b3
rekhansh99 <priyadarshirekhansh@gmail.com>
2020-05-03 01:27:37 +0530

    addCustomer cloud function implemented

---
+++ server/functions/routes/addCustomer.js
@@ -0,0 +1,27 @@
+const admin = require('../firebase');
+
+module.exports = async (data, context) => {
+  try {
+    const userRecord = await admin.auth().createUser(data);
+    await admin.firestore().doc("users/" + userRecord.uid).set({
+      displayName: data.displayName,
+      email: data.email,
+      mobileNo: data.phoneNumber,
+      cart: {
+        products: {},
+        discount: 0,
+        subTotal: 0,
+        total: 0
+      },
+      wishlist: [],
+      addresses: [],
+      orders: [],
+      coupons: [],
+      tags: []
+    });
+
+    return {success: true};
+  } catch (error) {
+    return {success: false};
+  }
+};
