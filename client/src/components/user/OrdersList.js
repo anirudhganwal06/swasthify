a12156bb2db4961f716b4ee3d0a03b6faff9e781
Anirudh <anirudhganwal06@gmail.com>
2020-06-06 17:57:09 +0530

    fixes #6

--- client/src/components/user/OrdersList.js
+++ client/src/components/user/OrdersList.js
@@ -10,27 +10,45 @@ class OrdersList extends Component {
   render() {
     const orderCards = [];
 
-    for(const order in this.props.orders) {
+    for (const order in this.props.orders) {
       orderCards.push(
-        <OrderCard key={order} orderid={order} order={this.props.orders[order]} />
+        <OrderCard
+          key={order}
+          orderid={order}
+          order={this.props.orders[order]}
+        />
       );
     }
 
     return (
       <div className="container p-3">
         <h1 className="text-center themeHeadingLg">My Orders</h1>
-        {isLoaded(this.props.orders) ? orderCards.reverse() : loading("80px")}
+        {isLoaded(this.props.orderIds) ? (
+          this.props.orderIds.length === 0 ? (
+            <div className="text-center">
+              <span className="far fa-frown fa-5x pt-3"></span>
+              <h5 className="text-center mt-3">You haven't ordered yet!</h5>
+            </div>
+          ) : isLoaded(this.props.orders) ? (
+            orderCards.reverse()
+          ) : (
+            loading("80px")
+          )
+        ) : (
+          loading("80px")
+        )}
       </div>
     );
   }
 }
 
-const getQuery = props => props.orderIds.map(id => ({
-  collection: "orders",
-  doc: id
-}));
+const getQuery = (props) =>
+  props.orderIds.map((id) => ({
+    collection: "orders",
+    doc: id
+  }));
 
-const matchStateToProps = state => ({
+const matchStateToProps = (state) => ({
   orderIds: state.firebase.profile.orders,
   orders: state.firestore.data.orders
 });
