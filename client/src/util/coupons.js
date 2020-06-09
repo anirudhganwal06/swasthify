eefbe11e44b1a039d23ee0feb546803a4c380e40
Anirudh <anirudhganwal06@gmail.com>
2020-06-09 22:18:31 +0530

    converted exports. syntax to export const

--- client/src/util/coupons.js
+++ client/src/util/coupons.js
@@ -1,4 +1,4 @@
-exports.discountValue = (user, coupon) => {
+export const discountValue = (user, coupon) => {
   let chancesLeft = 0;
 
   switch (coupon.customerEligibilityType) {
@@ -62,7 +62,7 @@ const calcDiscount = (cartSubTotal, coupon) => {
   }
 };
 
-exports.getCouponMessage = (coupon) => {
+export const getCouponMessage = (coupon) => {
   let message = "Get ";
 
   if (coupon.type === "Percentage") message += coupon.value + "% discount";
